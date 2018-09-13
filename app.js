d3c814413783c85336b7230949c0c32c28efc44b
Skazi_Premiere <skazi.web@yandex.ru>
2018-09-13 23:26:22 +0700

    add worker restart

--- app.js
+++ app.js
@@ -17,9 +17,16 @@ if (cluster.isMaster) {
     for (let i = 0; i < numCPUs; i++) {
         cluster.fork();
     }
-    cluster.on('exit', (worker, code, signal) => {
-        console.log(`worker ${worker.process.pid} died`);
-    });
+    cluster.on('exit', function(deadWorker, code, signal) {
+    // Restart the worker
+    var worker = cluster.fork();
+    // Note the process IDs
+    var newPID = worker.process.pid;
+    var oldPID = deadWorker.process.pid;
+    // Log the event
+    console.log('worker '+oldPID+' died.');
+    console.log('worker '+newPID+' born.');
+  });
 } else {
     app.use('/', routes);
     app.listen(port, () => {
